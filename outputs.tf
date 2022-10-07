7feaba65a5aa98a6804e2639586bb66b6f4e7103
Ray Ryjewski <brightblueray@users.noreply.github.com>
2022-10-07 14:43:35 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
-output "region" {
-  value = var.region
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
 
-output "prefix" {
-  value = var.prefix
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "bastion_instance_id" {
+  value = aws_instance.bastion.id
 }
